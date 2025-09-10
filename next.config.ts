f64ff9167e3b4eddc02c8ac81414e996900de327
samad <samad.cs010@gmail.com>
2025-09-10 23:28:02 +0530

    added security headers in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,39 @@
 import type { NextConfig } from 'next';
 
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: `
+      default-src 'self' * data: blob:;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' * data: blob:;
+      style-src 'self' 'unsafe-inline' * data:;
+      img-src 'self' * data: blob:;
+      font-src 'self' * data:;
+      connect-src 'self' *;
+      frame-src *;
+    `
+      .replace(/\s{2,}/g, ' ')
+      .trim(),
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
