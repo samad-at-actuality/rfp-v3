dc055699f6d200cd3f5d17288f19f9cb8a359621
samad <samad.cs010@gmail.com>
2025-09-15 04:49:58 +0530

    fixed access mgmt on files-table

--- src/components/files-table.tsx
+++ src/components/files-table.tsx
@@ -48,7 +48,8 @@ export const FilesTable = ({
   const {
     currentOrg: { role: crtOrgAccess },
   } = useOrgCtx();
-  const disableEdit = crtOrgAccess !== TOrgRole.ADMIN;
+  const isAdmin = crtOrgAccess === TOrgRole.ADMIN;
+  const isViewer = crtOrgAccess === TOrgRole.VIEWER;
 
   const [existingFiles, setExistingFiles] = useState<TFolderFile[]>(files);
 
@@ -70,198 +71,192 @@ export const FilesTable = ({
   }, [files]);
 
   return (
-    <>
-      {/* <pre>{JSON.stringify(files, null, 2)}</pre> */}
+    <div className='p-6 flex flex-col min-h-screen'>
+      <div className='space-y-6 flex-1'>
+        <Breadcrumb>
+          <BreadcrumbList className='text-[rgb(3.939% 3.939% 3.939%)]'>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link
+                  href={`/app/orgs/${orgId}/data`}
+                  className='text-2xl font-bold'
+                >
+                  Knowledge Hub
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link
+                  href={`/app/orgs/${orgId}/data/${primaryFolderSlug}`}
+                  className='text-2xl font-bold'
+                >
+                  {primaryFolderName}
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <span className='text-2xl font-bold'>{folderInfo_?.name}</span>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
 
-      <div className='p-6 flex flex-col min-h-screen'>
-        <div className='space-y-6 flex-1'>
-          <Breadcrumb>
-            <BreadcrumbList className='text-[rgb(3.939% 3.939% 3.939%)]'>
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <Link
-                    href={`/app/orgs/${orgId}/data`}
-                    className='text-2xl font-bold'
-                  >
-                    Knowledge Hub
-                  </Link>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <Link
-                    href={`/app/orgs/${orgId}/data/${primaryFolderSlug}`}
-                    className='text-2xl font-bold'
-                  >
-                    {primaryFolderName}
-                  </Link>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <span className='text-2xl font-bold'>
-                    {folderInfo_?.name}
-                  </span>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
+        <div>
+          <h3
+            style={{ color: '#6B7280', fontFamily: 'Inter' }}
+            className='mb-4'
+          >
+            {files.length} {files.length === 1 ? 'item' : 'items'}
+          </h3>
+        </div>
+        <div>
+          <h3
+            style={{
+              color: '#6B7280',
+              fontFamily: 'Inter',
+              fontWeight: '600',
+            }}
+            className='my-3'
+          >
+            Documents
+          </h3>
+        </div>
 
-          <div>
-            <h3
-              style={{ color: '#6B7280', fontFamily: 'Inter' }}
-              className='mb-4'
-            >
-              {files.length} {files.length === 1 ? 'item' : 'items'}
-            </h3>
-          </div>
-          <div>
-            <h3
-              style={{
-                color: '#6B7280',
-                fontFamily: 'Inter',
-                fontWeight: '600',
-              }}
-              className='my-3'
-            >
-              Documents
-            </h3>
-          </div>
+        <div className='overflow-x-auto rounded-xl  '>
+          <Table
+            style={{
+              fontFamily: 'Inter',
+              fontSize: '16px',
+              fontWeight: '500',
+            }}
+            className='w-full   text-left'
+          >
+            <TableHeader className='text-gray-500 border-b bg-gray-50'>
+              <TableRow>
+                <TableHead className='py-3 px-4'>
+                  <input type='checkbox' />
+                </TableHead>
+                <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
+                  File name
+                </TableHead>
+                <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
+                  Uploaded by
+                </TableHead>
+                <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
+                  Size
+                </TableHead>
+                <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
+                  Credential name
+                </TableHead>
+                <TableHead className='py-3 px-4 font-normal text-right text-md text-[#6B7280]'>
+                  Date
+                </TableHead>
+                <TableHead className='py-3 px-4 text-right font-normal text-md text-[#6B7280]'>
+                  Actions
+                </TableHead>
+              </TableRow>
+            </TableHeader>
 
-          <div className='overflow-x-auto rounded-xl  '>
-            <Table
-              style={{
-                fontFamily: 'Inter',
-                fontSize: '16px',
-                fontWeight: '500',
-              }}
-              className='w-full   text-left'
-            >
-              <TableHeader className='text-gray-500 border-b bg-gray-50'>
-                <TableRow>
-                  <TableHead className='py-3 px-4'>
-                    <input type='checkbox' />
-                  </TableHead>
-                  <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
-                    File name
-                  </TableHead>
-                  <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
-                    Uploaded by
-                  </TableHead>
-                  <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
-                    Size
-                  </TableHead>
-                  <TableHead className='py-3 px-4 font-normal text-md text-[#6B7280]'>
-                    Credential name
-                  </TableHead>
-                  <TableHead className='py-3 px-4 font-normal text-right text-md text-[#6B7280]'>
-                    Date
-                  </TableHead>
-                  <TableHead className='py-3 px-4 text-right font-normal text-md text-[#6B7280]'>
-                    Actions
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
+            <TableBody>
+              {existingFiles.map((doc, index) => {
+                const d = new Date(doc.createdAt);
+                const formattedDate = d.toLocaleDateString(undefined, {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                });
+                const formattedTime = d.toLocaleTimeString(undefined, {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                });
 
-              <TableBody>
-                {existingFiles.map((doc, index) => {
-                  const d = new Date(doc.createdAt);
-                  const formattedDate = d.toLocaleDateString(undefined, {
-                    month: 'long',
-                    day: 'numeric',
-                    year: 'numeric',
-                  });
-                  const formattedTime = d.toLocaleTimeString(undefined, {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                  });
-
-                  return (
-                    <TableRow key={index} className='border-b last:border-0'>
-                      <TableCell className='py-3 px-4'>
-                        <input type='checkbox' />
-                      </TableCell>
-                      <TableCell className='py-3 px-4 text-[#000000] text-md'>
-                        {doc.name}
-                      </TableCell>
-                      <TableCell className='py-3 px-4 text-md text-[#6B7280]'>
-                        XYZ
-                      </TableCell>
-                      <TableCell className='py-3 px-4 text-md text-[#6B7280]'>
-                        {formatFileSize(doc.size)}
-                      </TableCell>
-                      <TableCell className='py-3 px-4 text-md text-[#6B7280]'>
-                        ABC
-                      </TableCell>
-                      <TableCell className='py-3 px-4 text-right whitespace-nowrap text-md text-[#6B7280]'>
-                        {formattedDate}{' '}
-                        <span className='text-xs text-gray-400'>
-                          {formattedTime}
-                        </span>
-                      </TableCell>
-                      <TableCell className='py-3 px-4 text-right space-x-2'>
-                        {!disableEdit && (
-                          <>
-                            <FileDownloader
-                              fileId={doc.id}
-                              orgId={orgId}
-                              filaName={doc.name}
-                            />
-                            <FileDeleter
-                              fileId={doc.id}
-                              orgId={orgId}
-                              onDeleteCB={() => {
-                                setFiles((p) =>
-                                  p.filter((pp) => pp.id !== doc.id)
-                                );
-                              }}
-                            />
-                          </>
+                return (
+                  <TableRow key={index} className='border-b last:border-0'>
+                    <TableCell className='py-3 px-4'>
+                      <input type='checkbox' />
+                    </TableCell>
+                    <TableCell className='py-3 px-4 text-[#000000] text-md'>
+                      {doc.name}
+                    </TableCell>
+                    <TableCell className='py-3 px-4 text-md text-[#6B7280]'>
+                      XYZ
+                    </TableCell>
+                    <TableCell className='py-3 px-4 text-md text-[#6B7280]'>
+                      {formatFileSize(doc.size)}
+                    </TableCell>
+                    <TableCell className='py-3 px-4 text-md text-[#6B7280]'>
+                      ABC
+                    </TableCell>
+                    <TableCell className='py-3 px-4 text-right whitespace-nowrap text-md text-[#6B7280]'>
+                      {formattedDate}{' '}
+                      <span className='text-xs text-gray-400'>
+                        {formattedTime}
+                      </span>
+                    </TableCell>
+                    <TableCell className='py-3 px-4 text-right space-x-2'>
+                      <>
+                        <FileDownloader
+                          fileId={doc.id}
+                          orgId={orgId}
+                          filaName={doc.name}
+                        />
+                        {isAdmin && (
+                          <FileDeleter
+                            fileId={doc.id}
+                            orgId={orgId}
+                            onDeleteCB={() => {
+                              setFiles((p) =>
+                                p.filter((pp) => pp.id !== doc.id)
+                              );
+                            }}
+                          />
                         )}
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-              </TableBody>
-            </Table>
-          </div>
+                      </>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </div>
 
-          <div className='flex items-center gap-3 mt-4'>
-            {!disableEdit && (
-              <FileUploaderDialog
-                trigger={
-                  <Button
-                    style={{
-                      color: '#6B7280',
-                      backgroundColor: 'white',
-                      border: '1px solid #E5E7EB',
-                    }}
-                    ref={ref}
-                  >
-                    <Upload className='w-4 h-4' />
-                    Upload
-                  </Button>
+        <div className='flex items-center gap-3 mt-4'>
+          {!isViewer && (
+            <FileUploaderDialog
+              trigger={
+                <Button
+                  style={{
+                    color: '#6B7280',
+                    backgroundColor: 'white',
+                    border: '1px solid #E5E7EB',
+                  }}
+                  ref={ref}
+                >
+                  <Upload className='w-4 h-4' />
+                  Upload
+                </Button>
+              }
+              orgId={orgId}
+              folderId={folderInfo_?.id || ''}
+              type={folderInfo_.type}
+              onUpload={async (payloads) => {
+                try {
+                  const res = await uploadeMediaFiles(orgId, payloads);
+
+                  setExistingFiles((p) => [...p, ...(res?.data || [])]);
+                  return [];
+                } catch {
+                  return payloads;
                 }
-                orgId={orgId}
-                folderId={folderInfo_?.id || ''}
-                type={folderInfo_.type}
-                onUpload={async (payloads) => {
-                  try {
-                    const res = await uploadeMediaFiles(orgId, payloads);
-
-                    setExistingFiles((p) => [...p, ...(res?.data || [])]);
-                    return [];
-                  } catch {
-                    return payloads;
-                  }
-                }}
-              />
-            )}
-          </div>
+              }}
+            />
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 };
